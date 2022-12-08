320854866687cbef338c15ec39caa3ed57042d25
Peleg Admi <pelegadmi@gmail.com>
2022-12-08 16:59:18 +0200

    Updated User model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,14 +2,27 @@ import { model, Schema, Document } from 'mongoose';
 import { User } from '@interfaces/users.interface';
 
 const userSchema: Schema = new Schema({
-  email: {
-    type: String,
-    required: true,
+  id: {
+    type: Date,
+    require: true,
     unique: true,
   },
-  password: {
+  scenario_id: {
+    type: Date,
+    require: true,
+    unique: false, // it is not unique per user.
+  },
+  nickname: {
     type: String,
+    require: true,
+  },
+  scenario_start_time: {
+    type: Date,
     required: true,
+  },
+  messages: {
+    type: [String],
+    require: false, // might be false if user sent nothing.
   },
 });
 
