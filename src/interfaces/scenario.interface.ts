9e6d2c307d31f1c7cbfad8bff7ba214bc9347910
Peleg Admi <pelegadmi@gmail.com>
2022-12-13 16:56:29 +0200

    added scenario routes and minor fixture to userDto

--- src/interfaces/scenario.interface.ts
+++ src/interfaces/scenario.interface.ts
@@ -1,10 +1,11 @@
 import { Severity } from '@/enums/severity.enum';
 import { CommentType } from '@/enums/commentType.enum';
+import { ScenarioMessage } from '@interfaces/scenario-message.interface';
 
 export interface Scenario {
-  id: Date;
+  id: string;
   severity: Severity;
   commentType: CommentType;
   numberOfUsers: Number;
-  message: [];
+  message: [ScenarioMessage];
 }
