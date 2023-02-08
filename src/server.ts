09b6fb90ff9c7d9ccdaa56c8243f6deaf8ac6fc8
Peleg Admi <pelegadmi@gmail.com>
2023-02-08 17:53:52 +0200

    injected route via DI

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import UsersRoute from '@routes/users.route';
 import ScenariosRoute from '@routes/scenarios.route';
 import validateEnv from '@utils/validateEnv';
 import AdminRoute from '@routes/admin.route';
+import ScenarioReactionsRoute from '@routes/scenario-reactions.route';
 
 validateEnv();
 
-const app = new App([new UsersRoute(), new ScenariosRoute(), new AdminRoute()]);
+const app = new App([new UsersRoute(), new ScenariosRoute(), new AdminRoute(), new ScenarioReactionsRoute()]);
 
 app.listen();
